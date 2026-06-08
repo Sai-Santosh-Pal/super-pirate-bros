59d01e074796138022b91222ac5650c05e9bd594
Sai Santosh Pal <saisantoshpal2010@gmail.com>
2026-06-08 13:21:32 +0530

    player spawn created

--- code/main.py
+++ code/main.py
@@ -1,4 +1,7 @@
 from settings import *
+from level import Level
+from pytmx.util_pygame import load_pygame
+from os.path import join
 
 class Game:
     def __init__(self):
@@ -6,12 +9,17 @@ class Game:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Super Pirate World')
 
+        self.tmx_maps = {0: load_pygame(join('.', 'data', 'levels', 'omni.tmx'))}
+        self.current_stage = Level(self.tmx_maps[0])
+
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+            self.current_stage.run()
 
             pygame.display.update()
 
