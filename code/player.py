6736cdf55ecf437f95dd4e5ff14313cf64e88484
Sai Santosh Pal <saisantoshpal2010@gmail.com>
2026-06-09 10:22:33 +0530

    added wall jump and slide

--- code/player.py
+++ code/player.py
@@ -1,4 +1,5 @@
 from settings import *
+from timer import Timer
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
@@ -18,16 +19,20 @@ class Player(pygame.sprite.Sprite):
         self.collision_sprites = collision_sprites
         self.on_surface = {'floor': False, 'left': False, 'right': False}
 
-        self.display_surface = pygame.display.get_surface()
+        self.timers = {
+            'wall jump': Timer(400),
+            'wall slide block': Timer(250)
+        }
 
     def input(self):
         keys = pygame.key.get_pressed()
         input_vector = vector(0,0)
-        if keys[pygame.K_RIGHT]:
-            input_vector.x += 1
-        if keys[pygame.K_LEFT]:
-            input_vector.x -= 1
-        self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
+        if not self.timers['wall jump'].active:
+            if keys[pygame.K_RIGHT]:
+                input_vector.x += 1
+            if keys[pygame.K_LEFT]:
+                input_vector.x -= 1
+            self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
 
         if keys[pygame.K_SPACE]:
             self.jump = True
@@ -36,31 +41,35 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += self.direction.x * self.speed * dt
         self.collision('horizontal')
 
-        self.direction.y += self.gravity / 2 * dt
-        self.rect.y += self.direction.y * dt
-        self.direction.y += self.gravity / 2 * dt
-        self.collision('vertical')
+        if not self.on_surface['floor'] and any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['wall slide block'].active:
+            self.direction.y = 0
+            self.rect.y += self.gravity / 10 * dt
+        else:
+            self.direction.y += self.gravity / 2 * dt
+            self.rect.y += self.direction.y * dt
+            self.direction.y += self.gravity / 2 * dt
 
         if self.jump:
             if self.on_surface['floor']:
                 self.direction.y = -self.jump_height
+                self.timers['wall slide block'].activate()
+            elif any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['wall slide block'].active:
+                self.timers['wall jump'].activate()
+                self.direction.y = -self.jump_height
+                self.direction.x = 1 if self.on_surface['left'] else -1
             self.jump = False
+        
+        self.collision('vertical')
 
     def check_contact(self):
         floor_rect = pygame.Rect(self.rect.bottomleft,(self.rect.width,2))
         right_rect = pygame.Rect(self.rect.topright + vector(0,self.rect.height / 4),(2,self.rect.height / 2))
         left_rect  = pygame.Rect(self.rect.topleft + vector(-2,self.rect.height / 4),(2,self.rect.height / 2))
-
-        pygame.draw.rect(self.display_surface, 'yellow', floor_rect)
-        pygame.draw.rect(self.display_surface, 'yellow', right_rect)
-        pygame.draw.rect(self.display_surface, 'yellow', left_rect)
-
         collide_rects = [sprite.rect for sprite in self.collision_sprites]
 
         self.on_surface['floor'] = True if floor_rect.collidelist(collide_rects) >= 0 else False
         self.on_surface['right'] = True if right_rect.collidelist(collide_rects) >= 0 else False
         self.on_surface['left']  = True if left_rect.collidelist(collide_rects) >= 0 else False
-        print(self.on_surface)
 
     def collision(self, axis):
         for sprite in self.collision_sprites:
@@ -79,8 +88,13 @@ class Player(pygame.sprite.Sprite):
                         self.rect.bottom = sprite.rect.top
                     self.direction.y = 0
 
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
+
     def update(self, dt):
         self.old_rect = self.rect.copy()
+        self.update_timers()
         self.input()
         self.move(dt)
         self.check_contact()
