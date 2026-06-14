2d9604d707e2018471e0772a57e932a6dcfe18a2
Sai Santosh Pal <saisantoshpal2010@gmail.com>
2026-06-14 21:39:27 +0530

    basic enemmy animations done

--- code/enemies.py
+++ code/enemies.py
@@ -1,5 +1,6 @@
 from settings import *
 from random import choice
+from timer import Timer
 
 class Tooth(pygame.sprite.Sprite):
     def __init__(self, pos, frames, groups, collision_sprites):
@@ -28,12 +29,43 @@ class Tooth(pygame.sprite.Sprite):
             self.direction *= -1
 
 class Shell(pygame.sprite.Sprite):
-    def __init__(self, pos, frames, groups):
+    def __init__(self, pos, frames, groups, reverse, player):
         super().__init__(groups)
 
-        self.frames, self.frame_index = frames, 0
+        if reverse:
+            self.frames = {}
+            for key, surfs in frames.items():
+                self.frames[key] = [pygame.transform.flip(surf, True, False) for surf in surfs]
+            self.bullet_direction = -1
+        else:
+            self.frames = frames
+            self.bullet_direction = 1
+
+        self.frame_index = 0
         self.state = 'idle'
         self.image = self.frames[self.state][self.frame_index]
         self.rect = self.image.get_rect(topleft = pos)
         self.old_rect = self.rect.copy()
         self.z = Z_LAYERS['main']
+        self.player = player
+        self.shoot_timer = Timer(3000)
+
+    def state_management(self):
+        player_pos, shell_pos = vector(self.player.hitbox_rect.center), vector(self.rect.center)
+        player_near = shell_pos.distance_to(player_pos) < 500
+        player_front = shell_pos.x < player_pos.x if self.bullet_direction > 0 else shell_pos.x > player_pos.x
+        player_level = abs(shell_pos.y - player_pos.y) < 30
+
+        if player_near and player_front and player_level and not self.shoot_timer.active:
+            self.state = 'fire'
+            self.frame_index = 0
+            self.shoot_timer.activate()
+
+
+    def update(self, dt):
+        self.shoot_timer.update()
+        self.state_management()
+
+        self.frame_index += ANIMATION_SPEED * dt
+        if self.frame_index < len(self.frames[self.state]):
+            self.image = self.frames[self.state][int(self.frame_index)]
